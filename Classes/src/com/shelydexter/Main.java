a15815d02fca47de3756dd74bd857e25c8503c57
Michele <micheleannedexter@gmail.com>
2018-10-12 17:57:41 -0500

    classes

--- Classes/src/com/shelydexter/Main.java
+++ Classes/src/com/shelydexter/Main.java
@@ -3,6 +3,9 @@ package com.shelydexter;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println(porsche.getModel());
     }
 }
