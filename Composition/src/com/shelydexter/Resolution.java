1656fa2153869d4ef3ed2db8a88fb539023600cf
Michele <micheleannedexter@gmail.com>
2018-10-14 11:06:36 -0500

    composition setup

---
+++ Composition/src/com/shelydexter/Resolution.java
@@ -0,0 +1,20 @@
+package com.shelydexter;
+
+public class Resolution {
+
+    public int width;
+    public int height;
+
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
