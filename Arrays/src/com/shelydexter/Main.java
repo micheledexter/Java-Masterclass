404b32a7887cafdfbe432e34bfce091d6629616b
Michele <micheleannedexter@gmail.com>
2018-10-16 12:34:16 -0500

    arrays

--- Arrays/src/com/shelydexter/Main.java
+++ Arrays/src/com/shelydexter/Main.java
@@ -1,8 +1,23 @@
 package com.shelydexter;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] myIntArray = new int[25]; // {1,2,3,4,5,6,7,8,9,10};
+//        myIntArray[0] = 45;
+//        myIntArray[1] = 476;
+//        myIntArray[5] = 50;
+        for (int i=0; i<myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+        printArray(myIntArray);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i=0; i<array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
+        }
     }
 }
