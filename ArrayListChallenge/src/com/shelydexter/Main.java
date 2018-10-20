ead250f880e1e987f4c9b3e1c636fd83438d6868
Michele <micheleannedexter@gmail.com>
2018-10-20 13:55:23 -0500

    array list challenge

--- ArrayListChallenge/src/com/shelydexter/Main.java
+++ ArrayListChallenge/src/com/shelydexter/Main.java
@@ -1,6 +1,11 @@
 package com.shelydexter;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static MobilePhone mobile = new MobilePhone(null);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         // Create a program that implements a simple mobile phone with the following capabilities.
@@ -15,6 +20,87 @@ public class Main {
         // Be sure not to expose the inner workings of the ArrayList to MobilePhone
         // e.g. no ints, no .get(i) etc
         // MobilePhone should do everything with Contact objects only.
+        boolean running = true;
 
+        while (running) {
+            mainMenu();
+            System.out.print("Selection: ");
+            int selection = scanner.nextInt();
+            switch(selection) {
+                case 1:
+                    listContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    removeContact();
+                    break;
+                case 4:
+                    modifyContact();
+                    break;
+                case 5:
+                    findContact();
+                    break;
+                case 0:
+                    running = false;
+                    break;
+            }
+        }
+        scanner.close();
+    }
+
+    public static void mainMenu() {
+        System.out.println("Main menu:");
+        System.out.println("1 - List contacts");
+        System.out.println("2 - Add new contact");
+        System.out.println("3 - Remove a contact");
+        System.out.println("4 - Modify a contact");
+        System.out.println("5 - Find a contact");
+        System.out.println("0 - Quit");
+    }
+
+    public static void listContacts() {
+        mobile.listContacts();
+    }
+
+    public static void addNewContact() {
+        scanner.nextLine();
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Phone number: ");
+        String phone = scanner.nextLine();
+        mobile.addContact(name, phone);
+    }
+
+    public static void removeContact() {
+        mobile.listContacts();
+        System.out.print("Please enter the number of the contact you wish to remove: ");
+        int remove = scanner.nextInt();
+        mobile.removeContact(remove);
+    }
+
+    public static void modifyContact() {
+        mobile.listContacts();
+        System.out.print("Please enter the number of the contact you wish to modify: ");
+        int modify = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("New name: ");
+        String name = scanner.nextLine();
+        System.out.println("New phone number: ");
+        String phone = scanner.nextLine();
+        mobile.updateContact(modify, name, phone);
+    }
+
+    public static void findContact() {
+        scanner.nextLine();
+        System.out.println("Please enter the name of the contact you wish to find:");
+        String name = scanner.nextLine();
+        int index = mobile.findContact(name);
+        if (index < 0) {
+            System.out.println("Contact not found.");
+        } else {
+            System.out.println("Contact found, listed as contact #" + index);
+        }
     }
 }
--- ArrayListChallenge/src/com/shelydexter/MobilePhone.java
+++ ArrayListChallenge/src/com/shelydexter/MobilePhone.java
@@ -1,5 +1,64 @@
-package com.shelydexter;;
+package com.shelydexter;
+
+import java.util.ArrayList;
 
 public class MobilePhone {
 
+    private ArrayList<Contact> contacts;
+
+    public MobilePhone(ArrayList<Contact> contacts) {
+        this.contacts = new ArrayList<Contact>();
+    }
+
+    public int findContact(String name) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Contact findContact(int index) {
+        return contacts.get(index);
+    }
+
+    public void addContact(String name, String phone) {
+        if (findContact(name) >= 0) {
+            System.out.println("Contact already exists!");
+        } else {
+            contacts.add(new Contact(name, phone));
+            System.out.println("Contact " + name + " with phone number " + phone + " added to contact list.");
+        }
+    }
+
+    public void removeContact(int index) {
+        if (index < 0 || index >= contacts.size()) {
+            System.out.println("No contact found");
+        } else {
+            Contact contact = contacts.get(index);
+            contacts.remove(contact);
+            System.out.println("Contact " + contact.getName() + " with phone number " + contact.getPhone() + " removed.");
+        }
+    }
+
+    public void updateContact(int index, String newName, String newPhone) {
+        Contact contact = findContact(index);
+        if (index >= 0) {
+            removeContact(index);
+            addContact(newName, newPhone);
+            System.out.println("Contact updated.");
+        } else {
+            System.out.println("Contact " + contact.getName() + " could not be update: Not found.");
+        }
+    }
+
+    public void listContacts() {
+        for (int i = 0; i < contacts.size(); i++) {
+            Contact contact = contacts.get(i);
+            String name = contact.getName();
+            String phone = contact.getPhone();
+            System.out.println(i + " -  Name: " + name + " Phone #: " + phone);
+        }
+    }
 }
