4f00a8599e8b97a4b2c863bc0a86118405f4ca02
Michele <micheleannedexter@gmail.com>
2018-11-04 09:27:55 -0600

    linked list

--- LinkedList/src/com/shelydexter/Demo.java
+++ LinkedList/src/com/shelydexter/Demo.java
@@ -2,6 +2,7 @@ package com.shelydexter;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     public static void main(String[] args) {
@@ -29,5 +30,29 @@ public class Demo {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("=========================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if (comparison == 0) {
+                // equal, do not add
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            } else if (comparison > 0) {
+                // new City should appear before this one
+                // Brisbane -> Adelaide
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                // move onto next city
+            }
+        }
+
+        stringListIterator.add(newCity);
+        return true;
     }
 }
